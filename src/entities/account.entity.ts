b581783c2fd606ebb65ee05ee5b589ca3475a069
Petrik <petrikdiak@petrik.hu>
2023-03-29 12:00:58 +0200

    entity

---
+++ src/entities/account.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Account {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  accountNumber: string;
+
+  @Column()
+  balance: number;
+}
---
+++ src/entities/owner.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Owner {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  fullName: string;
+
+  @Column()
+  business: number;
+}
